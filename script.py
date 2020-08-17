8961fafdf45c668bc86051b8f4acabd527fc77c9
ericlee <lee3388@purdue.edu>
2020-08-16 23:26:54 -0400

    :bug: Fix bug in script

--- script.py
+++ script.py
@@ -8,5 +8,5 @@ def run(cmd):
 FILENAME = 'expTestD'
 Ds = [1, 2.25, 10]
 for D in Ds:
-    run('python3 allenChan.py --filename --D {} --iteration 500 --interval 10'.format(FILENAME, D))
+    run('python3 allenChan.py --filename {} --D {} --iteration 500 --interval 10'.format(FILENAME, D))
 
