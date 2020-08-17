ee125c27bcaeee953741ee10c087ad3e6a775f8e
ericlee <lee3388@purdue.edu>
2020-08-16 23:24:03 -0400

    :construction: Update script

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 import os
 
+
+def run(cmd):
+    print(cmd)
+    os.system(cmd)
+
 FILENAME = 'expTestD'
 Ds = [1, 2.25, 10]
 for D in Ds:
-    os.system('python3 allenChan.py --D {} --iteration 500 --interval 10'.format(D))
+    run('python3 allenChan.py --filename --D {} --iteration 500 --interval 10'.format(FILENAME, D))
 
