d6addc13c063e79551b1544c3c9eae3ece2febef
ericlee <lee3388@purdue.edu>
2020-08-16 22:31:51 -0400

    :construction: Add parameter control

--- allenChan.py
+++ allenChan.py
@@ -32,9 +32,12 @@ if __name__ == '__main__':
     # deal with parameters
     parser = argparse.ArgumentParser(description='Allen Cahn Model Simulator')
     parser.add_argument('--filename', default='test', help='Output Path Directory')
-    parser.add_argument('--iteration', default=10000, type=int, help='Number of Iteration')
+    parser.add_argument('--iteration', default=1000, type=int, help='Number of Iteration')
     parser.add_argument('--interval', default=500, type=int, help='Interval Output Plot')
+    parser.add_argument('--D', default=2.25, type=float, help='Diffusion Constant')
+    parser.add_argument('--alpha', default=0.015, type=float, help='alpha')
     args = parser.parse_args()
+    paramStr = 'D_{}_alpha_{}'.format(args.D, args.alpha)
 
     # initialize mesh
     nx = ny = 500
@@ -45,12 +48,12 @@ if __name__ == '__main__':
     dt = 5e-4
 
     # heat flux equation (D is the diffusion constant)
-    D = 2.25
+    D = args.D
     phase = CellVariable(name=r'$\phi$', mesh=mesh, hasOld=True)
     dT = CellVariable(name=r'$\Delta T$', mesh=mesh, hasOld=True)
     heatEq = (TransientTerm() == DiffusionTerm(D) + (phase - phase.old) / dt)
 
-    alpha = 0.015
+    alpha = args.alpha
     c = 0.02
     N = 6.0
     theta = numerix.pi / 8.0
@@ -70,18 +73,21 @@ if __name__ == '__main__':
     kappa2 = 20.
     phaseEq = (TransientTerm(tau)
                == DiffusionTerm(D)
-               + ImplicitSourceTerm((phase - 0.5 - kappa1 / numerix.pi * numerix.arctan(kappa2 * dT))
+                           + ImplicitSourceTerm((phase - 0.5 - kappa1 / numerix.pi * numerix.arctan(kappa2 * dT))
                                     * (1 - phase)))
 
     radius = dx * 5.
     C = (nx * dx / 2, ny * dy / 2)
     x, y = mesh.cellCenters
+
+    # set initial condition
     phase.setValue(1., where=((x - C[0]) ** 2 + (y - C[1]) ** 2) < radius ** 2)
     dT.setValue(-0.5)
 
     steps = args.iteration
     viewer = DentriteViewer(phase=phase, dT=dT, title=r"%s & %s" % (phase.name, dT.name), datamin=-0.1, datamax=0.05)
-    Path(args.filename).mkdir(parents=True, exist_ok=True)
+
+    Path('/tmp/' + args.filename).mkdir(parents=True, exist_ok=True)
     Path('result').mkdir(parents=True, exist_ok=True)
     for i in range(steps):
         phase.updateOld()
@@ -89,8 +95,8 @@ if __name__ == '__main__':
         phaseEq.solve(phase, dt=dt)
         heatEq.solve(dT, dt=dt)
         if i % args.interval == 0:
-            viewer.plot(filename='{}/{}.png'.format(args.filename, i))
+            viewer.plot(filename='/tmp/{}/{}_{}.png'.format(args.filename, paramStr, i))
         # final step
         if i == (steps - 1):
-            viewer.plot(filename='{}/result.png'.format(args.filename))
-            viewer.plot(filename='result/{}.png'.format(args.filename))
+            viewer.plot(filename='/tmp/{}/result_{}.png'.format(args.filename, paramStr))
+            viewer.plot(filename='result/{}_{}.png'.format(args.filename, paramStr))
